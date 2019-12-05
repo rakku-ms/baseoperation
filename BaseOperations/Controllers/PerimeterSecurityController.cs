0609b6dc7acd4956f7b1b83a259d3071ec38665d
Anjay Ajodha <anajod@microsoft.com>
2019-12-05 06:25:27 -0800

    Initial Commit

---
+++ BaseOperations/Controllers/PerimeterSecurityController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BaseOperations.Controllers
+{
+    public class PerimeterSecurityController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Retraining()
+        {
+            return View();
+        }
+
+        public IActionResult ApplyModel()
+        {
+            return View();
+        }
+    }
+}
