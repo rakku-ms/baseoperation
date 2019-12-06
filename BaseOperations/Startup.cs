ba16e5061f80fdd1df59722a0c39517eb0aacbc3
Anjay Ajodha <anajod@microsoft.com>
2019-12-06 09:08:58 -0800

    UI Updates

--- BaseOperations/Startup.cs
+++ BaseOperations/Startup.cs
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.VisualStudio.Web.BrowserLink;
+using Microsoft.AspNetCore.Mvc.Razor.RuntimeCompilation;
 
 namespace BaseOperations
 {
@@ -23,7 +25,7 @@ namespace BaseOperations
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
+            services.AddControllersWithViews().AddRazorRuntimeCompilation();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -32,6 +34,7 @@ namespace BaseOperations
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseBrowserLink();
             }
             else
             {
